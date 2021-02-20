50cf9c9af02d77c73771983c79e7cf04d6dab230
Emmanuel Okoye <developeradmin@genesystechhub.com>
2021-02-20 23:08:21 +0100

    migrations enabled

---
+++ Payment_System.Domain/Migrations/20210220191325_initial.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Payment_System.Domain.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentStatus",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentStatus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreditCardNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CardHolder = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PaymentStatusId = table.Column<int>(type: "int", nullable: false),
+                    PaymentStatusId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payment_PaymentStatus_PaymentStatusId1",
+                        column: x => x.PaymentStatusId1,
+                        principalTable: "PaymentStatus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_PaymentStatusId1",
+                table: "Payment",
+                column: "PaymentStatusId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payment");
+
+            migrationBuilder.DropTable(
+                name: "PaymentStatus");
+        }
+    }
+}
